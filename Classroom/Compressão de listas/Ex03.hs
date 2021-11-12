d98b267ab3ba2805c3bcadcc3a5f15dce165bd59
IgorLeo01 <94202863+IgorLeo01@users.noreply.github.com>
2021-11-12 20:40:34 -0300

    Create Ex03.hs

---
+++ Classroom/Compressão de listas/Ex03.hs
@@ -0,0 +1,9 @@
+grid :: Int -> Int -> [(Int,Int)]
+grid x y = [(x,y) | x <- [0..x], y <- [0..y]]
+
+gridIguais :: Int -> [(Int,Int)]
+quadrado n = [(x,y) | (x,y) <- grid n n, x == y ]
+
+main :: IO ()
+main = do
+    print $ gridIguais 2
